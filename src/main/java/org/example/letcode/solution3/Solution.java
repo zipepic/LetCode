52c6ea323196b75d9859abfabaa6347701cdb2d2
zipepic <olezhka.kirilov.97@mail.ru>
2023-11-28 17:57:51 +0300

    Refactored Solution 3

--- src/main/java/org/example/letcode/solution3/Solution.java
+++ src/main/java/org/example/letcode/solution3/Solution.java
@@ -6,36 +6,7 @@ import java.util.Map;
 class Solution {
     public static void main(String[] args) {
         System.out.println(new Solution().lengthOfLongestSubstring("au"));
-//        new Solution().lengthOfLongestSubstring("aaaaab");
     }
-//    public int lengthOfLongestSubstring(String s) {
-//        Map<Integer,String> map = new HashMap<>();
-//        int i =0;
-//        int max=-1;
-//        String buff = "";
-//        while (i<s.length()){
-//            if(buff.contains(s.charAt(i)+"")){
-//                map.put(buff.length(),buff);
-//                if(max<buff.length())
-//                    max = buff.length();
-//                if(buff.charAt(0) == s.charAt(i)){
-//                    buff=buff.substring(1);
-//                }else {
-//                    buff=s.charAt(i)+"";
-//                }
-//                i--;
-//            }else {
-//                buff+=s.charAt(i);
-//            }
-//            System.out.println("i ->" + i+" buff ->"+buff);
-//            i++;
-//        }
-//        map.put(buff.length(),buff);
-//        if(max<buff.length())
-//            max = buff.length();
-//        System.out.println(map);
-//        return max;
-//    }
     public int lengthOfLongestSubstring(String s){
         for (int i=0;i<s.length();i++){
             int frameSize = s.length()-i;
