709f1b59d3ba003a6595a63fb604699d2dc9132d
xzz1p <olezhka.kirilov.97@mail.ru>
2023-11-29 18:44:07 +0300

    Solution 35

---
+++ src/main/java/org/example/letcode/solution35/Solution.java
@@ -0,0 +1,24 @@
+package org.example.letcode.solution35;
+
+class Solution {
+    public static void main(String[] args) {
+        System.out.println(new Solution().searchInsert(new int[]{1,3,5,6},7));
+    }
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length;
+        int mid = nums.length/2;
+        while (left<right){
+            mid = left+ (right-left)/2;
+            int number = nums[mid];
+            if(number<target){
+                left = mid+1;
+            }else if(number>target){
+                right = mid-1;
+            }else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
