ad0dd22e4b70107363a85467947fcd11f1fc321e
xzz1p <olezhka.kirilov.97@mail.ru>
2023-12-03 00:00:27 +0300

    Solution 88

--- src/main/java/org/example/letcode/solution88/Solution.java
+++ src/main/java/org/example/letcode/solution88/Solution.java
@@ -1,26 +1,23 @@
 package org.example.letcode.solution88;
 
+import java.util.Arrays;
+
 public class Solution {
     public static void main(String[] args) {
-        int[] arr = new int[]{1,2,3,0,0,0};
+        int[] arr = new int[]{4,5,6,0,0,0};
         Solution solution = new Solution();
-        solution.merge(arr,3,new int[]{2,5,6},3);
+        solution.merge(arr,3,new int[]{1,2,3},3);
         solution.print(arr);
     }
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i =0;
         int j =0;
-        int[] mergedArray = new int[m+n];
-        while (i<m || j<n){
-            if(nums1[i] < nums2[j] && i<m){
-                mergedArray[i+j]=nums1[i];
-                i++;
-            }else if(j<n) {
-                mergedArray[i+j] = nums2[j];
-                j++;
-            }
+        for(int i =0;i<nums1.length;i++){
+           if(j<n&&nums1[i]==0){
+               nums1[i] = nums2[j];
+               j++;
+           }
         }
-       nums1 = mergedArray;
+        Arrays.sort(nums1);
     }
     void print(int[] nums){
         for (int num : nums) {
